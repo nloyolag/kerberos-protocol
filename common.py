e59e8e86c7a20e644d99fb54610a2ceb34d6fcd3
Noe Loyola <noeloyolag@gmail.com>
2015-10-25 19:37:42 -0600

    Finished service server response, added helper functions to common.

--- common.py
+++ common.py
@@ -1,1 +1,40 @@
+import base64
+from Crypto.Cipher import AES
+from Crypto.Hash import MD5
+from Crypto import Random
+
+#
 # File to declare functions and classes used by servers and client
+#
+
+# Block size in bytes of AES Message
+BLOCK_SIZE = 16
+
+# Function to pad data that does not match BLOCK_SIZE, used by AES methods
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s)-1:])]
+
+# Functions to encrypt and decrypt AES
+def encrypt_aes(plaintext, key):
+    plaintext = pad(plaintext)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(plaintext))
+
+def decrypt_aes(ciphertext, key):
+    ciphertext = base64.b64decode(ciphertext)
+    iv = ciphertext[:16]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(ciphertext[16:]))
+
+# Function that returns the hash of a given plaintext
+def md5_hash(plaintext):
+    h = MD5.new()
+    h.update(plaintext)
+    return h
+
+# Class for the authenticator object to be sent through the procol
+class Authenticator:
+    def __init__(self, clientID, timestamp):
+        self.clientID = clientID
+        self.timestamp = timestamp
--- service_server.py
+++ service_server.py
@@ -1,26 +1,68 @@
 import socket
+import thread
+import sys
+import pickle
 import common
 
 HOST = ''
 PORT = 8888
+PRIVATE_KEY = '1234'
+SESSION_KEY = '1234'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(10)
-
-while True:
-    # Wait for connection from client
-    connection, address = s.accept()
+# Function executed for each client attempting to connect
+def connection_thread(connection):
+    connection.send("Connection Established. Waiting for message E and G...\n")
 
     # Receive 2 messages:
     #     Message E: Client to server ticket encryted with services secret key
     #     Message G: New authenticator(clientID, Timestamp) encrypted with session key
-    me = connection.recv(4096)
-    mg = connection.recv(4096)
+    message_e = connection.recv(4096)
+    message_g = connection.recv(4096)
 
-# Decrypt ticket with SS secret key to retrieve session key
-# Decrypt authenticator with session key
-# Send message to Client
-#     Message H: Timestamp in clients authenticator encrypted with session key
-#
-# Wait for requests from Client
+    message_e = pickle.loads(message_e)
+    message_g = pickle.loads(message_g)
+
+    # Decrypt ticket with SS secret key to retrieve session key
+    ticket = common.decrypt_aes(message_e)
+
+    # Decrypt authenticator with session key
+    authenticator = common.decrypt_aes(message_g)
+
+    # Send message to Client
+    #     Message H: Timestamp in clients authenticator encrypted with session key
+    message_h = common.encrypt_aes(authenticator.timestamp, SESSION_KEY)
+    message_h = pickle.dumps(message_h)
+    connection.sendall(message_h)
+
+    # Wait for requests from Client
+    while True:
+        data = conn.recv(4096)
+        reply = 'Received ' + data
+        if not data:
+            break
+
+        connection.sendall(reply)
+
+    connection.close()
+
+
+if __name__ == "__main__":
+
+    # Creation of socket for connection with client
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.bind((HOST, PORT))
+    except socket.error:
+        print 'Socket Bind Error'
+        sys.exit()
+
+    # Wait for connections from client
+    s.listen(10)
+
+    while True:
+        connection, address = s.accept()
+        print "New connection with " + address[0] + ':' + str(address[1])
+        thread.start_new_thread(connection_thread, (connection,))
+
+    s.close()
