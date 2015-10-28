a8eba9571e954b44d580a53294bfe0f62078e943
marccolm <marcolm485@gmail.com>
2015-10-28 10:57:15 -0600

    Finshed client(?)

--- client.py
+++ client.py
@@ -1,23 +1,25 @@
-import socket
+from socket import socket, AF_INET, SOCK_DGRAM
 import thread
 import sys
 import pickle
 import common
 import getpass
+import time
 
 # Change timestamps
+# User/Password
 
 HOST = ''
 PORT = 8888
-#PRIVATE_KEY = '1234'
-#SESSION_KEY = '1234'
+TGS_IP = '10.25.76.86'
+SS_IP = ''
 
 user_credentials = dict([('user', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8')])
 
 def check_password(clear_password, password_hash):
-	return SHA256.new(clear_password).hexdigest() == password_hash
+	return common.SHA256.new(clear_password).hexdigest() == password_hash
 
-def connection(connection):
+def tgs_connection(connection):
 	""" Communication with TGS """
 	# Client sends cleartext message with the user id requesting services
 	connection.send('user')
@@ -31,6 +33,7 @@ def connection(connection):
 	# Message C composed with TGT and ID of requested service
 	message_c = common.MessageC(message_b, 'service')
 	# Message D authenticator with id and timestamp
+	timestamp = time.time
 	message_d = common.MessageD('user', timestamp)
 	encrypted_message_d = encrypt_aes(message_d, session_key)
 	# Send message c and d
@@ -40,14 +43,15 @@ def connection(connection):
 	message_e = session.recv(4096)
 	message_f = session.recv(4096)
 
-	ss_connection(connection, message_e, message_f)
-
+	messages = [message_e, message_f]
+	return messages
 
 def ss_connection(connection, message_e, message_f):
 	""" Communication with SS """
 	# Client connects to the SS and sends message e encrypted with service's key and g encrypted using session key
 	connection.send(message_e)
-	message_g = common.MessageG('user', timestamp)
+	timestamp = time.time
+	message_g = common.MessageD('user', timestamp)
 	encrypted_message_g = encrypt_aes(message_g, session_key)
 	# Receives message h to confirm identity
 	encrypted_message_h = session.recv(4096)
@@ -60,7 +64,13 @@ if __name__ == "__main__":
 	password = getpass.getpass('Password: ')
 	if check_password(password, user_credentials[user]):
 		print('Login successful')
-		connection(connection)
-
+		socket = socket(AF_INET, SOCK_DGRAM)
+		socket.bind((TGS_IP, PORT))
+		messages = tgs_connection(socket)
+		message_e = messages[0]
+		message_f = messages[1]
+		socket.bind((SS_IP, PORT))
+		ss_connection(socket, message_e, message_f)
+		socket.close()
 	else:
 		print('Invalid credentials')
