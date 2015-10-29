bf3a53d5f281d39062797ad663f952e076aaade5
marccolm <marcolm485@gmail.com>
2015-10-29 17:27:44 -0600

    Conexión con irving auth server terminada

--- client.py
+++ client.py
@@ -12,6 +12,7 @@ import time
 HOST = ''
 PORT = 8888
 AUTH_IP = '10.25.76.2'
+SS_IP = '10.25.75.176'
 
 user_credentials = dict([('user', common.sha256_hash('1234123412341234').hexdigest()[0:16])])
 #user_credentials['user'] = common.sha256_hash('1234123412341234').hexdigest()[0:16]
@@ -24,42 +25,61 @@ def tgs_connection(connection):
 	connection.connect((AUTH_IP, PORT))
 	print 'Connected successfully to ip ' + AUTH_IP
 	connection.sendall('user')
+
 	# Receive Client/TGS Session key encrypted using the secret key of the client
+
 	message_a = connection.recv(4096)
+
 	# Receive Ticket-Granting-Ticket encrypted using the key of the TGS
+
 	message_b = connection.recv(4096)
+
 	# Decrypt session key with secret key of client
+
 	decrypted_message_a = common.decrypt_aes(message_a, user_credentials['user'])
 	session_key = decrypted_message_a.sessionKey
+
 	# Message C composed with TGT and ID of requested service
+
 	message_c = common.MessageC(message_b, 'service')
+
 	# Message D authenticator with id and timestamp
-	timestamp = time.time
+
+	timestamp = time.time()
 	message_d = common.MessageD('user', timestamp)
 	encrypted_message_d = common.encrypt_aes(message_d, session_key)
+
 	# Send message c and d
+
 	message_c = pickle.dumps(message_c)
 	connection.sendall(message_c)
 	connection.sendall(encrypted_message_d)
+
 	# Receive messages e and f from TGS
+
 	message_e = connection.recv(4096)
 	message_f = connection.recv(4096)
-
 	messages = [message_e, message_f]
 	return messages
 
 def ss_connection(connection, message_e, message_f):
 	""" Communication with SS """
 	# Client connects to the SS and sends message e encrypted with service's key and g encrypted using session key
+	connection.connect((SS_IP, PORT))
+	print 'Connected successfully to ip ' + AUTH_IP
 	connection.sendall(message_e)
 	timestamp = time.time
 	message_g = common.MessageD('user', timestamp)
-	encrypted_message_g = encrypt_aes(message_g, session_key)
+	encrypted_message_g = common.encrypt_aes(message_g, session_key)
+
 	# Receives message h to confirm identity
+
 	connection.listen(10)
 	encrypted_message_h = client.recv(4096)
+
 	# Decrypt confirmation and check timestamp
-	message_h = decrypt_aes(encrypted_message_h, session_key)
+
+	message_h = common.decrypt_aes(encrypted_message_h, session_key)
 	# Server provides service
 
 if __name__ == "__main__":
@@ -69,6 +89,7 @@ if __name__ == "__main__":
 	print 'Login successful'
 	
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	"""try:
 		s.bind((HOST, PORT))
@@ -80,8 +101,8 @@ if __name__ == "__main__":
 	messages = tgs_connection(s)
 	message_e = messages[0]
 	message_f = messages[1]
-	s.bind((HOST, PORT))
-	ss_connection(s, message_e, message_f)
+
+	ss_connection(ss, message_e, message_f)
 	s.close()
 	#else:
 	#	print('Invalid credentials')
