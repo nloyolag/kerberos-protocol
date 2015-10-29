d1acb9ce2caf7534cc604b79e108426586644937
Noe Loyola <noeloyolag@gmail.com>
2015-10-29 17:41:14 -0600

    Fixed weird commit

--- auth_server.py
+++ auth_server.py
@@ -3,47 +3,67 @@ import thread
 import sys
 import pickle
 import common
+import time
 
-database = dict([('user', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), ('service',''),('tgs','')])
+#user:password
+database = dict([('user', ''), ('service',''),('tgs','')])
 CLIENT_TGS_SESSION = '1234123412341234'
 CLIENT_SERVICE_SESSION = '1234123412341234'
+HOST = ""
+PORT = 8888
+
 
 def connection_thread(connection):
-    #Receive client ID
-    client_ID = connection.recv(4096)
-    #Get hashed password from database
-    client_secretK = database.get(client_ID,None)
-    message_a = common.MessageA(CLIENT_TGS_SESSION)
-    message_a = common.encrypt_aes(message_a,client_secretK)
-    #Set message B with TGT= clientID, ip address, lifetime, client/TGS sessionkey encrypted with secret TGS
-    message_b = common.MessageB(client_ID,lifetime,CLIENT_TGS_SESSION)
-    message_b = common.encrypt_aes(message_b,database.get('tgs'))
-    #Send message A and B to client
-    connection.sendall(message_a)
-    connection.sendall(message_b)
-    #receive message C with message B(TGT) and service id
-    message_c = connection.recv(4096)
-    #receive message D with authenticator(clientID,timestamp) encrypted with client/TGT session key
-    message_d = connection.recv(4096)
-    #Re serialize object from stream
-    message_c = pickle.loads(message_c)
-    message_d = pickle.loads(message_d)
-    #open message C to get message B and service id
-    #Can i do this ?  or decrypt message_c.ticket
-    message_b = common.MessageB(message_c.ticket.clientId,message_c.ticket.validityPeriod,message_c.ticket.clientSessionKey)
-    message_b = common.decrypt_aes(message_b,database.get('tgs'))
-    #Decrypt message D with with client/TGS session key
-    message_d = common.decrypt_aes(message_d,message_b.clientSessionKey)
-    #Create message E(client/server ticket(clientID,clientIP,lifetime,client/TGS session key)) encrypted with service sercret key
-    message_e = common.MessageB(client_ID,lifetime,CLIENT_SERVICE_SESSION)
-    message_e = common.encrypt_aes(message_e,database.get('service'))
-    #Create message F(client/server session key) encrypted with client/tgs session key
-    message_f = common.MessageF(CLIENT_SERVICE_SESSION)
-    message_f = common.encrypt_aes(message_f,message_b.clientSessionKey)
-    #Send message E
-    connection.sendall(message_e)
-    connection.sendall(message_f)
+	#Set private key of service server
 
+	database['service'] = common.sha256_hash('1234123412341234').hexdigest()[0:16]
+	#set private key of tgs
+	database['tgs'] = common.sha256_hash('1234123412341234').hexdigest()[0:16]
+	#Set private key of user
+	database['user'] = common.sha256_hash('1234123412341234').hexdigest()[0:16]
+	#lifetime for messages to be valid
+	lifetime = 3600
+    #Receive client ID
+	client_ID = connection.recv(4096)
+    #Get hashed password from database
+	client_secretK = database[client_ID]
+
+	message_a = common.MessageA(CLIENT_TGS_SESSION)
+	message_a = common.encrypt_aes(message_a,client_secretK)
+	#Set message B with TGT= clientID, ip address, lifetime, client/TGS sessionkey encrypted with secret TGS
+	message_b = common.MessageB(client_ID,lifetime,CLIENT_TGS_SESSION)
+	message_b = common.encrypt_aes(message_b,database['tgs'])
+	#Send message A and B to client
+	connection.sendall(message_a)
+	connection.sendall(message_b)
+	#receive message C with message B(TGT) and service id
+	message_c = connection.recv(4096)
+	#receive message D with authenticator(clientID,timestamp) encrypted with client/TGT session key
+	message_d = connection.recv(4096)
+	#Re serialize object from stream only C because it is not encrypted
+	message_c = pickle.loads(message_c)
+	#message_d = pickle.loads(message_d) decrypt_aes does this
+	#open message C to get message B and service id
+	message_b = common.decrypt_aes(message_c.ticket,database['tgs'])
+	#message_b = common.MessageB(message_b.clientId,message_b.validityPeriod,message_b.clientSessionKey)
+	#message_b = common.decrypt_aes(message_b,database['tgs'])
+	#Decrypt message D with with client/TGS session key
+	message_d = common.decrypt_aes(message_d,message_b.clientSessionKey)
+	#Check validity period of message D.timestamp with B.lifetime
+	if message_d.timestamp+message_b.validityPeriod > time.time():
+	    #Create message E(client/server ticket(clientID,clientIP,lifetime,client/TGS session key)) encrypted with service sercret key
+	    message_e = common.MessageB(client_ID,lifetime,CLIENT_SERVICE_SESSION)
+	    message_e = common.encrypt_aes(message_e,database['service'])
+	    #Create message F(client/server session key) encrypted with client/tgs session key
+	    message_f = common.MessageF(CLIENT_SERVICE_SESSION)
+	    message_f = common.encrypt_aes(message_f,message_b.clientSessionKey)
+	    #Send message E
+	    connection.sendall(message_e)
+	    connection.sendall(message_f)
+	else:
+		print "Validity period of message D is not valid."
+
+	print "Server finished"
 
 
 if __name__ == "__main__":
